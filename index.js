a448b573fed556036d127208d0fc584fc7d2d60e
Megan Byers <megan@designelixir.studio>
2024-10-16 00:04:01 -0600

    rework function

--- index.js
+++ index.js
@@ -1,23 +1,41 @@
-function togglePopup(id = null) {
+var titles = [
+    'Volume I - Horace and Augusta Teaser',
+    'Volume II - Elizabeth McCourt "Baby" Doe Teaser',
+    'Volume III - Wealth, Politics and Scandal Teaser',
+    'Volume IV - The Best of Times Demands Repentance Teaser'
+];
+
+var videos = [
+    'videos/Tabor-Vol-1-Website.mp4',
+    'videos/Tabor-Vol-2-Website.mp4',
+    'videos/Tabor-Vol-3-Website.mp4',
+    'videos/Tabor-Vol-4-Website.mp4'
+];
+
+function togglePopup(index) {
     const popupWindow = $('#Popup');
-    const popupTeasers = $('.popup-teaser');
+    const popupTeaser = $('.popup-teaser');
+    const videoElement = $('#Popup video');
+    const popupTitle = $('#popupTitle');
 
     if (popupWindow.hasClass('closed')) {
         // Open the popup
         popupWindow.removeClass('closed').css('opacity', 0).animate({ opacity: 1 }, 300);
 
-        // Hide all teaser sections first
-        popupTeasers.addClass('hidden');
+        // Set the title and video source using the provided index
+        popupTitle.text(titles[index]);
+        videoElement.attr('src', videos[index]);
 
-        // Show the specific section by ID if provided
-        if (id) {
-            $(`#${id}`).removeClass('hidden');
-        }
+        // Show the popup teaser
+        popupTeaser.removeClass('hidden');
     } else {
         // Close the popup with fade-out effect
         popupWindow.animate({ opacity: 0 }, 300, function () {
             $(this).addClass('closed');
-            popupTeasers.addClass('hidden'); // Hide all teaser sections when closing
+
+            // Remove the title and video source when closing
+            popupTitle.text('');
+            videoElement.attr('src', '');
         });
     }
 }
